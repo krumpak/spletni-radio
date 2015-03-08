3a31fb2503ec32357823df33c645e9c43c2ff7cf
krumpak <gorazd.krumpak@gmail.com>
2015-03-08 21:59:31 +0100

    spletni-radio.dev [2015-03-08 21:59:30] commit v0.1.0

--- dev/script.js
+++ dev/script.js
@@ -30,64 +30,78 @@ function AllRadioStations()
         for(i = 0; i < listData.length; i++)
         {
             if(listData[i].ime.toLowerCase().indexOf(checkedStr)>=0){
-            list += "<li><a href='#' onclick='ChangeCookieRadioStationValue(\""+listData[i].kodno+"\");'>" + listData[i].ime+"</a></li>";
+            list += "<li><a href='#' onclick='ChangeRadioStation(\""+listData[i].kodno+"\");'>" + listData[i].ime+"</a></li>";
         }
     }
     list += "</ul>"
     $("postaje").innerHTML = list;
 }
 
-function setCookie(cValue)
+function setLS(lsStation, lsTheme, lsValoume)
 {
-    var cName = "spletni-radio";
-    var d = new Date();
-    d.setTime(d.getTime() + (1*24*60*60*1000));
-    var expires = "expires="+d.toUTCString();
-    document.cookie = cName + "=" + cValue + "; " + expires;
+    localStorage.setItem("RadioStation", lsStation);
+    localStorage.setItem("RadioTheme", lsTheme);
+    localStorage.setItem("RadioVolume", lsValoume);
 }
 
-function getCookieValues()
+function getLS_Station()
 {
-    var cName = "spletni-radio";
-    var cContent = document.cookie.replace(cName+"=", "").split(':');
-    return cContent;
+    var LS_value = localStorage.getItem("RadioStation");
+    var value = localStorage.getItem("RadioStation"); 
+    if( LS_value == '' || LS_value == null || LS_value == undefined ){ value = ''; }
+    return value;  
 }
 
-function getCookieRadioStation()
+function getLS_Theme()
 {
-    var cValue = getCookieValues();
-    return cValue[0];
+    var LS_value = localStorage.getItem("RadioTheme");
+    var value = localStorage.getItem("RadioTheme");
+    if( LS_value == '' || LS_value == null || LS_value == undefined ){ value = ''; }
+    return value; 
 }
 
-function getCookieTheme()
+function getLS_Volume()
 {
-    var cValue = getCookieValues();
-    return cValue[1];
+    var LS_value = localStorage.getItem("RadioVolume");
+    var value = localStorage.getItem("RadioVolume");
+    if( LS_value == '' || LS_value == null || LS_value == undefined ){ value = ''; }
+    return value; 
 }
 
-function setCookieRadioStationValue(RadioStation)
+function setLS_RadioStationValue(RadioStation)
 {
-    return RadioStation+":"+getCookieTheme()+":"+getCookieVolume();
+    return localStorage.setItem("RadioStation", RadioStation);
 }
 
-function setCookieThemeValue(Theme)
+function setLS_ThemeValue(RadioTheme)
 {
-    return getCookieRadioStation()+":"+Theme+":"+getCookieVolume();
+    return localStorage.setItem("RadioTheme", RadioTheme);
+}
+
+function setLS_VolumeValue(RadioVolume)
+{
+    return localStorage.setItem("RadioVolume", RadioVolume);
+}
+
+function ChangeRadioStation(RadioStation)
+{
+    setLS_RadioStationValue(RadioStation);
+    window.location.replace("/?"+RadioStation);
 }
 
 function ChangeTheme(Theme)
 {
-    setCookie( setCookieThemeValue(Theme) );
+    setLS_ThemeValue(Theme);
     $("tema").className = Theme;
 }
 
-function ChangeCookieRadioStationValue(RadioStation)
+function ChangeVolume(Volume)
 {
-    setCookie( setCookieRadioStationValue(RadioStation) );
-    window.location.replace("/?"+RadioStation);
+    setLS_VolumeValue(Volume);
+    $('streamer').volume = Volume;
 }
 
-function getUrlRadioStation()
+function getUrlStation()
 {
     return window.location.search.substring(1);
 }
@@ -100,79 +114,85 @@ function PlayRadio(stream) {
     audioElement.play();
 }
 
-function checkSettings() 
+function initiateRadio() 
 {
-    var Theme = getCookieTheme();
-    $("tema").className = Theme;
-    var urlRadioStation = getUrlRadioStation();
-    var CookieRadioStation = getCookieRadioStation();
-    var SelectedRadioStation ='';
+    if(typeof(Storage) == "undefined") {
+		$('RadionName') = "Browser does not support this radio!";
+		alert('ne podpira');
+	}
 
-    if( urlRadioStation != '' && CookieRadioStation == '' ){
-        SelectedRadioStation =urlRadioStation;
-        setCookie(SelectedRadioStation+":bela:0.7");
-    }
-    else if( urlRadioStation != '' && CookieRadioStation != '' ){
-        SelectedRadioStation =urlRadioStation;
-        setCookie(SelectedRadioStation+":bela:"+getCookieVolume());
-    } else {
-        SelectedRadioStation = CookieRadioStation;
-        if( getUrlRadioStation() != SelectedRadioStation )
-        {
-            window.location.replace("/?"+SelectedRadioStation);
-        }
-    }
+	var URL_Station = getUrlStation();
+	var LS_Station = getLS_Station();
+	var LS_Theme = getLS_Theme();
+	var LS_Volume = getLS_Volume();
 
-    if( typeof SelectedRadioStation == 'string' && SelectedRadioStation.length > 1 && SelectedRadioStation != "0")
-    {
-        var AllRadioStations=RadioStationList();
-        for(var i = 0; i < AllRadioStations.length; i++)
-        {
-            if(AllRadioStations[i].kodno==SelectedRadioStation)
-            {
-                var StreamURL=(AllRadioStations[i].stream);
-                if($('streamer') == null)
-                {
-                    PlayRadio(StreamURL);
-                    $('RadionName').innerHTML = "<a href='?"+AllRadioStations[i].kodno+"' target='_self'>"+AllRadioStations[i].ime+"</a>";
-                    document.title = AllRadioStations[i].ime;
-                }
-            }
-        }
-    } else {
-        setCookie("0:bela:0.7");
-    }
 
-    var cVolume = getCookieVolume();
-    cVolume = cVolume;
-    if( cVolume >= 0 && cVolume <= 1)
-    {
-        $('streamer').volume = cVolume;
-        var max = 40;
-        var bar = max*cVolume;
-        for(var i=1;i<=max;i++){
-            var element = "bar"+i;
-            if(i<=bar){
-                $(element).className = 'volumeBar over';
-            } else {
-                $(element).className = 'volumeBar';
-            }
-        }
-    } else {
-        setCookieVolumeValue(cVolume);
-    }
+	if( URL_Station != '' && LS_Station == '' && LS_Theme == '' &&  LS_Volume == '' )
+	{
+		setLS( URL_Station, "bela", "0.5" );
+		location.reload(); 
+	}
+	else if( URL_Station != '' && LS_Station != '' && LS_Theme != '' && LS_Volume != '' && URL_Station != LS_Station && URL_Station != '0' )
+	{
+		setLS( URL_Station, LS_Theme, LS_Volume );
+		location.reload();
+	}
+	else if( URL_Station == '' && LS_Station != '' && LS_Theme != '' && LS_Volume != '' && LS_Station != '0' )
+	{
+		window.open("/?"+LS_Station, '_self');
+	} 
+	else if( LS_Station != '' && LS_Theme != '' &&  LS_Volume != '' && LS_Station != '0' )
+	{
+	    LS_Theme = getLS_Theme();
+	    $("tema").className = LS_Theme;
+
+		var AllRadioStations=RadioStationList();
+	    for(var i = 0; i < AllRadioStations.length; i++)
+	    {
+	        if( AllRadioStations[i].kodno == LS_Station )
+	        {
+	            var StreamURL = AllRadioStations[i].stream;
+	            if($('streamer') == null)
+	            {
+	                PlayRadio(StreamURL);
+	                $('RadionName').innerHTML = "<a href='?"+AllRadioStations[i].kodno+"' target='_self'>"+AllRadioStations[i].ime+"</a>";
+	                document.title = AllRadioStations[i].ime;
+	            }
+	        }
+	    }
+
+	    var cVolume = getLS_Volume();
+	    if( cVolume >= 0 && cVolume <= 1)
+	    {
+	        $('streamer').volume = cVolume;
+	        var max = 40;
+	        var bar = max*cVolume;
+	        for(var i=1;i<=max;i++){
+	            var element = "bar"+i;
+	            if(i<=bar){
+	                $(element).className = 'volumeBar over';
+	            } else {
+	                $(element).className = 'volumeBar';
+	            }
+	        }
+	    } else {
+	        setLS_VolumeValue(cVolume);
+	    }
+	} else {
+		setLS( "0", "bela", "0.5" );
+	} 
 }
 
 function StartStopRadio(){
     if( $('streamer').paused )
     {
         $('streamer').play();
-        $('predvajaj').style.display='block';
+        $('predvajaj').style.display='inline-block';
         $('ustavi').style.display='none';
     } else {
         $('streamer').pause();
         $('predvajaj').style.display='none';
-        $('ustavi').style.display='block';
+        $('ustavi').style.display='inline-block';
     }
 }
 
@@ -180,10 +200,10 @@ function muteRadio(){
     if( $('streamer').muted )
     {
         $('mute').style.display='none';
-        $('muted').style.display='block';
+        $('muted').style.display='inline-block';
         $('streamer').muted = false;
     } else {
-        $('mute').style.display='block';
+        $('mute').style.display='inline-block';
         $('muted').style.display='none';
         $('streamer').muted = true;
     }
@@ -206,24 +226,6 @@ function VolumeSlider(){
         Slider += '<div id="bar'+i+'" onmousedown="ChangeMouseVolumeSlider('+i+','+NumberOfColumns+')" class="volumeBar" style="height:'+parseInt(SliderHeight)+'px;width:'+barWidth+'px;margin-top:'+parseInt(SliderTop-100+height)+'px"></div>';
     }
     $('volume').innerHTML = Slider;
-}
-
-function getCookieVolume()
-{
-    var cValue = getCookieValues();
-    return cValue[2];
-}
-
-function setCookieVolumeValue(Volume)
-{
-    return getCookieRadioStation()+":"+getCookieTheme()+":"+(parseFloat(Volume).toFixed(2));
-}
-
-function ChangeVolume(Volume)
-{
-    $('streamer').volume = Volume;
-    var cContent = setCookieVolumeValue(Volume);
-    setCookie( cContent );
 }
 
 function ChangeMouseVolumeSlider(bar,max){
